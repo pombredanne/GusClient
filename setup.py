06d8741568cde75b5d32f75b7a898f279bf690ae
Shawn Crosby <scrosby@salesforce.com>
2013-07-22 13:56:15 -0300

    fixed manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gus_client',
-    version='0.1.1',
+    version='0.1.2',
     author='Shawn Crosby',
     author_email='scrosby@salesforce.com',
     packages=find_packages(),
