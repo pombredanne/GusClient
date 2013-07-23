c41b526a597433713e029557dfc1b54e1a118e08
Shawn Crosby <scrosby@salesforce.com>
2013-07-23 17:17:53 -0300

    Update change log and test remote @reviewers ccasey

--- gus/Gus.py
+++ gus/Gus.py
@@ -4,18 +4,26 @@ import getpass
 
 class Client:
     sf_session = None
+    sf_session_id = None
     
-    def __init__(self):
-        session = GusSession()
-        try:
-            self.__create_session__(session.load_session_id())
-        except Exception as e:
-            print e
-            user = self.__prompt__('GUS Username', session.load_user_name())
-            passwd = getpass.getpass('Please enter your GUS password: ')
-            token = self.__prompt__('Security Token', session.load_gus_token())
-                
-            self.__create_session__(session.login(user, passwd, token))
+    def __init__(self, session_id=None):
+        if session_id is not None: #remote initialization
+            try:
+                self.__create_session__(session_id)
+            except Exception as e:
+                print "Unable to Create Session with passed token: %s" % session_id
+                print e
+        else: #local initialization
+            session = GusSession()
+            try:
+                self.__create_session__(session.load_session_id())
+            except Exception as e:
+                print e
+                user = self.__prompt__('GUS Username', session.load_user_name())
+                passwd = getpass.getpass('Please enter your GUS password: ')
+                token = self.__prompt__('Security Token', session.load_gus_token())
+                    
+                self.__create_session__(session.login(user, passwd, token))
             
     def __prompt__(self, prompt, default):
         if default is not None:
@@ -28,6 +36,10 @@ class Client:
     def __create_session__(self, session_id):
         self.sf_session = Salesforce(instance_url='https://gus.salesforce.com', session_id=session_id)
         self.sf_session.query("select Id from ADM_Work__c where Name='NOTHING'") #See if the token is good
+        self.sf_session_id = session_id
+        
+    def session_id(self):
+        return self.sf_session_id
 
 class NoRecordException(Exception):
     """nothing"""
