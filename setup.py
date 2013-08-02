f0131448b6b4173c3d6855a811b6283899e494dd
Shawn Crosby <scrosby@salesforce.com>
2013-08-02 15:51:22 -0300

    updated setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gus_client',
-    version='0.1.8',
+    version='0.1.9',
     author='Shawn Crosby',
     author_email='scrosby@salesforce.com',
     packages=find_packages(),
