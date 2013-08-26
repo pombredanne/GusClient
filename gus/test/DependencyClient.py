e5f0e8320fb83ca36cec0f2942f749f09a7c1015
Shawn Crosby <scrosby@salesforce.com>
2013-08-26 09:31:19 -0300

    refactor graph generator out of script and into module for testing

---
+++ gus/test/DependencyClient.py
@@ -0,0 +1,18 @@
+'''
+Created on Aug 26, 2013
+
+@author: scrosby
+'''
+import unittest
+from ..DependencyClient import DependencyClient, DependencyGrapher
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
