d3afe57e79785666d70ce38c2462943bb215147b
Shawn Crosby <scrosby@salesforce.com>
2013-09-04 09:54:20 -0300

    moved dependency graphing to its own project

--- setup.py
+++ setup.py
@@ -2,19 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='gus_client',
-    version='0.2.3',
+    version='0.2.4',
     author='Shawn Crosby',
     author_email='scrosby@salesforce.com',
     packages=find_packages(),
     license='Keep it real',
     description='Connect to GUS',
     long_description=open('README.txt').read(),
-    scripts=[
-             'bin/team_dependencies.py',
-             'bin/release_dependencies.py'],
     install_requires=[
         "simple_salesforce >= 0.51",
         "sc_pylibs>=0.1.1",
-        "pydot",
     ],
 )
